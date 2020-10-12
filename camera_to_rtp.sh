46cea2ffbfc713c4d6c3d5b359edf390083ea36a
Carl <carl@carldenslow.com>
2020-10-12 16:02:00 -0600

    added -v to help generate sdp file (if needed)

--- camera_to_rtp.sh
+++ camera_to_rtp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 CLIENT_IP=192.168.1.110
-gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1' ! omxh264enc control-rate=2 bitrate=4000000 ! video/x-h264, stream-format=byte-stream ! rtph264pay mtu=1400 ! udpsink host=$CLIENT_IP port=5000 sync=false async=false
+gst-launch-1.0 nvarguscamerasrc ! 'video/x-raw(memory:NVMM), width=1280, height=720, framerate=30/1' ! omxh264enc control-rate=2 bitrate=4000000 ! video/x-h264, stream-format=byte-stream ! rtph264pay mtu=1400 ! udpsink host=$CLIENT_IP port=5000 sync=false async=false -v
 
